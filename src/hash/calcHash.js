1b17fbe46642d8ba88fdd6471fabbb5db719de2b
Alexey Shevtsov <a.shevtsov@itransition.com>
2022-11-30 16:00:13 +0100

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { stdout } from "node:process";
+import { open } from "node:fs/promises";
+import { pathExist } from "../fs/fsCheck.mjs";
+import { FS_OPERATION_FAILED } from "../fs/fsErrorMessages.mjs";
+
+const { createHash } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileToReadPath = new URL(
+    "./files/fileToCalculateHashFor.txt",
+    import.meta.url
+  );
+  const IsFileToReadExist = await pathExist(fileToReadPath);
+
+  if (!IsFileToReadExist) {
+    throw new Error(FS_OPERATION_FAILED);
+  }
+
+  const fileHandle = await open(fileToReadPath);
+  const inputStream = fileHandle.createReadStream();
+
+  const hash1 = createHash("sha256");
+  inputStream.pipe(hash1).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
