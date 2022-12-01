7f240ab229cfc425b0259f2fbf88fe19d74f43a9
Alexey Shevtsov <a.shevtsov@itransition.com>
2022-12-01 19:54:26 +0100

    feat: implement decompress.js - implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { promisify } from "node:util";
+import { pathExist } from "../fs/fsCheck.mjs";
+import { FS_OPERATION_FAILED } from "../fs/fsErrorMessages.mjs";
+
 const decompress = async () => {
-    // Write your code here 
+  const gzippedFilePath = new URL("./files/archive.gz", import.meta.url);
+  const fileToUnzipPath = new URL("./files/fileToCompress.txt", import.meta.url);
+  const IsFileToReadExist = await pathExist(gzippedFilePath);
+
+  if (!IsFileToReadExist) {
+    throw new Error(FS_OPERATION_FAILED);
+  }
+
+  const ungzip = createUnzip();
+  const source = createReadStream(gzippedFilePath);
+  const destination = createWriteStream(fileToUnzipPath);
+  const pipe = promisify(pipeline);
+
+  await pipe(source, ungzip, destination);
 };
 
 await decompress();
