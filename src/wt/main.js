65d8b22188b101e8fe8352af1dd7b92033290cb1
Alexey Shevtsov <a.shevtsov@itransition.com>
2022-12-02 22:43:36 +0100

    feat: implement main.js - implement function that creates number of worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+import { STATUS_ERROR } from "../fs/fsErrorMessages.mjs";
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPath = new URL("./worker.js", import.meta.url);
+  let fibonacciN = 10;
+  const error = {
+    status: STATUS_ERROR,
+    data: null,
+  };
+
+  const workerPromise = () =>
+    new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, {
+        workerData: fibonacciN,
+      });
+
+      worker.on("message", resolve);
+      worker.on("error", reject);
+
+      fibonacciN++;
+    });
+  const workersPromises = cpus().map(workerPromise);
+  const results = await Promise.allSettled(workersPromises);
+
+  results.map((result) => {
+    if (result.value) {
+      console.info(result.value);
+    } else {
+      console.error(error);
+    }
+  });
 };
 
 await performCalculations();
