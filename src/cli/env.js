e6bb47f99c2cf232a07c512275fef735318bfaa0
Alexey Shevtsov <a.shevtsov@itransition.com>
2022-12-04 18:14:52 +0100

    feat: implement src/cp.js - implement function spawnChildProcess that receives array of arguments args and creates child process from file script.js, passing these args to it

--- src/cli/env.js
+++ src/cli/env.js
@@ -5,7 +5,6 @@ const parseEnv = () => {
 
   for (let envName in envs) {
     if (envs.hasOwnProperty(envName) && envName.substring(0, 4) === "RSS_") {
-      // console.log(`${envName}=${envs[envName]};`);
       // Add ";" to an output if it not a first env.
       output += output ? "; " : "";
       // Add evn with value to the output.
--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,30 @@
+import { spawn } from "node:child_process";
+import { createInterface } from "node:readline";
+import { stdin as input, stdout as output } from "node:process";
+import { pathExist } from "../fs/pathExist.mjs";
+import { FS_OPERATION_FAILED } from "../fs/fsErrorMessages.mjs";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childFilePath = new URL("./files/script.js", import.meta.url);
+  const IsChildFileExist = await pathExist(childFilePath);
+
+  if (!IsChildFileExist) {
+    throw new Error(FS_OPERATION_FAILED);
+  }
+
+  const preparedArgs = Array.isArray(args) ? args : [args];
+  const argsToChild = [childFilePath.pathname, ...preparedArgs];
+  const childProcess = spawn(process.argv[0], argsToChild);
+
+  const readline = createInterface({ input, output, terminal: false });
+
+  readline.on("line", (line) => {
+    // child process stdin should receive input from master process stdin.
+    childProcess.stdin.write(line);
+  });
+
+  // child process stdout should send data to master process stdout
+  childProcess.stdout.pipe(output);
 };
 
 spawnChildProcess();
